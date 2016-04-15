3127988658b6acb59de365e308a55a181127840c
Smudge <nathan@ngriffith.com>
2016-04-15 15:34:12 -0400

    Support domains that don't exist or have no spf record

--- spf_counter.rb
+++ spf_counter.rb
@@ -1,14 +1,16 @@
 #!/usr/bin/env ruby
 
 require 'spf/query'
+require 'active_support/core_ext/object/try'
 
 def count(domain)
-  SPF::Query::Record.query(domain).reduce(1) do |m, e|
-    m + (e.name == :include ? count(e.value) : 0)
+  return unless (query = SPF::Query::Record.query(domain))
+  query.reduce(1) do |m, e|
+    m + (e.name == :include ? (count(e.value) || 1) : 0)
   end
 end
 
 ARGV.each do |domain|
-  result = count(domain) - 1 # top level doesn't count
-  puts [domain, result, (result > 10 ? 'FAIL' : 'PASS')].join(', ')
+  result = count(domain).try(:-, 1)
+  puts [domain, result, ((result > 10 ? 'FAIL' : 'PASS') if result)].join(', ')
 end
